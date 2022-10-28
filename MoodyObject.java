35083c1ee8f33bd8f1865d3c27aaf06fd448eee9
kakingrizal <114561982+kakingrizal@users.noreply.github.com>
2022-10-28 21:34:05 +0700

    Update MoodyObject.java

--- MoodyObject.java
+++ MoodyObject.java
@@ -6,33 +6,3 @@ public class MoodyObject {
         System.out.println("I am : "+getMood());
     }
 }
-
-class SadObject {
-    protected String getSad(){
-        return "sad";
-    }
-    public void cry(){
-        System.out.println("Hoo hoo : "+ getSad());
-    }
-}
-
-class HappyObject {
-     protected String getHappy(){
-        return"happy";
-    }
-    public void laugh(){
-        System.out.println("Hahaha : " + getHappy());
-    }       
-}
-
-class MoodyTest {
-    public static void main(String[] args) {
-        MoodyObject m = new MoodyObject();
-        SadObject Sad = new SadObject();
-        HappyObject Happy = new HappyObject();
-        
-        m.speak();
-        Sad.cry();
-        Happy.laugh();
-    }
-}
