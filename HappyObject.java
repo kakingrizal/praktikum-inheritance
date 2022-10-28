7f2c3276a4a98564edbba7e11cd7280db59786df
kakingrizal <114561982+kakingrizal@users.noreply.github.com>
2022-10-28 21:34:45 +0700

    Add files via upload

---
+++ HappyObject.java
@@ -0,0 +1,8 @@
+public class HappyObject {
+     protected String getHappy(){
+        return"happy";
+    }
+    public void laugh(){
+        System.out.println("Hahaha : " + getHappy());
+    }       
+}
---
+++ SadObject.java
@@ -0,0 +1,8 @@
+public class SadObject {
+    protected String getSad(){
+        return "sad";
+    }
+    public void cry(){
+        System.out.println("Hoo hoo : "+ getSad());
+    }
+}
